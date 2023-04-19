df079a372cba824017758ae03cfea054df1fe767
LiuFeng <liufeng@lami>
2023-04-19 14:22:26 +0800

    add login request schema

--- src/palschema/interface_schema.py
+++ src/palschema/interface_schema.py
@@ -2,6 +2,21 @@ from dataclasses import dataclass
 
 
 @dataclass
+class LoginRequestSchema:
+    email: str
+
+
+
+@dataclass
+class LoginResponseSchema:
+    uid: str
+    email: str
+    master_key_encrypted: str
+    private_key_encrypted: str
+    access_token_encrypted: str
+
+
+@dataclass
 class SearchThingRequestSchema:
     pass
 
--- src/palschema/user_schema.py
+++ src/palschema/user_schema.py
@@ -2,11 +2,6 @@ from dataclasses import dataclass
 
 
 @dataclass
-class LoginSchema:
-    email: str
-
-
-@dataclass
 class AuthSchema:
     access_token: str
     expired_ts: int
