0aaeb3dd1918613b475c6da0cb168aa137a6905d
LiuFeng <liufeng@lami>
2023-04-19 14:02:35 +0800

    add interface schema

---
+++ src/palschema/interface_schema.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class SearchThingRequestSchema:
+    pass
+
+
+@dataclass
+class SearchThingResponseSchema:
+    pass
+
+
+@dataclass
+class RequestThingRequestSchema:
+    pass
+
+
+@dataclass
+class RequestThingResponseSchema:
+    pass
+
+
+@dataclass
+class DownloadThingRequestSchema:
+    pass
+
+
+@dataclass
+class DownloadThingResponseSchema:
+    pass
