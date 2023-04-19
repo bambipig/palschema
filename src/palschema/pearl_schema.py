242763e2376cd72e0779161df76f9a40a065e561
LiuFeng <liufeng@lami>
2023-04-19 13:47:58 +0800

    init palschema

---
+++ src/palschema/pearl_schema.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from dataclasses import  field
+from typing import List
+
+
+@dataclass
+class ThingPieceSchema:
+    bytes_size: int
+    sha256_hash: str
+    piece_idx: int
+    dialect: str
+    storage_uri: str
+
+
+@dataclass
+class ThingSchema:
+    ttype_uuid: str
+    source_uuid: str
+    source_uri: str
+    thing_uuid: str
+    uri: str
+
+    pieces: List[ThingPieceSchema] = field(default_factory=list)
+
